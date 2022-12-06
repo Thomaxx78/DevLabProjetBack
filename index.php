b96740a5fdb6dd07d15d8ebbf00c79961bc38bef
Thomas Fi <tom.filhol@gmail.com>
2022-12-06 10:12:43 +0100

    :bug: Replace directory

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<header>
+    <a href="index.php">Accueil</a>
+    <a href="register.php">S'inscrire</a>
+    <a href="login.php">Se connecter</a>
+</header>
+<body>
+<h1> Bienvenue </h1>
+</body>
