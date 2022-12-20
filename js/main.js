e487535da9a2eb38ea2bf72a025fb6df95bbb066
RENAUD Brevin <renaudbrevin@gmail.com>
2022-12-20 15:32:40 +0100

    :sparkles: New sort system

--- js/main.js
+++ js/main.js
@@ -81,6 +81,7 @@ function showMovie(parent, film, movie){
     <img class="w-32" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Poster">
     <span class="text-center">${title}</span>
     <button class="detailsMovie text-white" value="${movie.id}">Movie Details</button>
+    <span class="hidden">${movie.vote_average}</span>
     `
     parent.appendChild(film);
     film.classList.add("flex", "flex-col", "items-center", "rounded", "shadow", "m-2", "p-2", "w-64", "bg-white", "text-black", "hover:bg-gray-200", "hover:text-gray-800", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
@@ -105,66 +106,102 @@ selectSort.addEventListener('change', function() {
 
 
 // Trier les films par nom
-function moviesByName(genre = 28) {
-    axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=e5be04ec7de9aff432b14905a60c0bb8')
-    .then((response) => {
-        let movies = response.data.results;
-        let allMovieTitles = [];
-        let title = "";
-        movies.forEach(movie => {
-            if(movie.title == null){
-                title = movie.name
-            } else {
-                title = movie.title
-            }
-            allMovieTitles.push(title);
-        })
-        allMovieTitles.sort();
-        document.querySelectorAll('.divParent > div').forEach(e => e.remove());
-        showMoviesSort(allMovieTitles, movies, "title")
+// function moviesByName() {
+//     axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=e5be04ec7de9aff432b14905a60c0bb8')
+//     .then((response) => {
+//         let movies = response.data.results;
+//         let allMovieTitles = [];
+//         let title = "";
+//         movies.forEach(movie => {
+//             if(movie.title == null){
+//                 title = movie.name
+//             } else {
+//                 title = movie.title
+//             }
+//             allMovieTitles.push(title);
+//         })
+//         allMovieTitles.sort();
+//         document.querySelectorAll('.divParent > div').forEach(e => e.remove());
+//         showMoviesSort(allMovieTitles, movies, "title")
+//     })
+// }
+
+
+// function moviesByPopularity(how){
+//     axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=e5be04ec7de9aff432b14905a60c0bb8')
+//     .then((response) => {
+//         let movies = response.data.results;
+//         let allMovieMark = [];
+//         movies.forEach(movie => {
+//             allMovieMark.push(movie.vote_average);
+//         })
+//         allMovieMark.sort();
+//         if(how == "Down"){
+//             allMovieMark.reverse();
+//         }
+//         // console.log(allMovieMark)
+//         document.querySelectorAll('.divParent > div').forEach(e => e.remove());
+//         showMoviesSort(allMovieMark, movies, "mark")
+//     })
+// }
+
+// // Ajouter les films dans l'ordre (Pas optimisé mais fonctionnel)
+// function showMoviesSort(array, movies, element){
+//     array.forEach(MovieOrder => {
+//         movies.forEach(movie => {
+//             let parent = document.querySelector('.divParent');
+//             let film = document.createElement('div');
+//             let title = "";
+//             if(movie.title == null){
+//                 title = movie.name
+//             } else {
+//                 title = movie.title
+//             }
+//             if(element == "mark"){
+//                 if(MovieOrder == movie.vote_average){
+//                     showMovie(parent, film, movie)
+//                 }
+//             } else {
+//                 if(title == MovieOrder){
+//                     showMovie(parent, film, movie)
+//                 }
+//             }
+//         })
+//     })
+// }
+
+
+function moviesByName(){
+    let allMovies = document.querySelectorAll('.divParent > div');
+    let allMovieTitles = [];
+    let allMoviesDictionary = {};
+    allMovies.forEach(movie => {
+        allMovieTitles.push(movie.children[1].innerHTML);
+        allMoviesDictionary[movie.children[1].innerHTML] = movie;
+    })
+    allMovieTitles.sort();
+    document.querySelectorAll('.divParent > div').forEach(e => e.remove());
+    allMovieTitles.forEach(title => {
+        document.querySelector('.divParent').appendChild(allMoviesDictionary[title]);
     })
 }
-
 
 function moviesByPopularity(how){
-    axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=e5be04ec7de9aff432b14905a60c0bb8')
-    .then((response) => {
-        let movies = response.data.results;
-        let allMovieMark = [];
-        movies.forEach(movie => {
-            allMovieMark.push(movie.vote_average);
-        })
-        allMovieMark.sort();
-        if(how == "Down"){
-            allMovieMark.reverse();
-        }
-        // console.log(allMovieMark)
-        document.querySelectorAll('.divParent > div').forEach(e => e.remove());
-        showMoviesSort(allMovieMark, movies, "mark")
+    let allMovies = document.querySelectorAll('.divParent > div');
+    let allMovieMarks = [];
+    let allMoviesDictionary = {};
+    let allMoviesDictionaryMark = {};
+    allMovies.forEach(movie => {
+        allMovieMarks.push(movie.children[3].innerHTML);
+        allMoviesDictionaryMark[movie.children[3].innerHTML] = movie.children[1].innerHTML;
+        allMoviesDictionary[movie.children[1].innerHTML] = movie;
     })
-}
-
-// Ajouter les films dans l'ordre (Pas optimisé mais fonctionnel)
-function showMoviesSort(array, movies, element){
-    array.forEach(MovieOrder => {
-        movies.forEach(movie => {
-            let parent = document.querySelector('.divParent');
-            let film = document.createElement('div');
-            let title = "";
-            if(movie.title == null){
-                title = movie.name
-            } else {
-                title = movie.title
-            }
-            if(element == "mark"){
-                if(MovieOrder == movie.vote_average){
-                    showMovie(parent, film, movie)
-                }
-            } else {
-                if(title == MovieOrder){
-                    showMovie(parent, film, movie)
-                }
-            }
-        })
+    allMovieMarks.sort();
+        if(how == "Down"){
+            allMovieMarks.reverse();
+        }
+    document.querySelectorAll('.divParent > div').forEach(e => e.remove());
+    allMovieMarks.forEach(mark => {
+        document.querySelector('.divParent').appendChild(allMoviesDictionary[allMoviesDictionaryMark[mark]]);
     })
 }
