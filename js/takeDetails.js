90f3f895cc67868d71823c9e70765544c64dd1df
Thomas Fi <tom.filhol@gmail.com>
2022-12-20 13:39:54 +0100

    :bug: merging

---
+++ js/takeDetails.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const {
+    host, hostname, href, origin, pathname, port, protocol, search
+} = window.location
+
+console.log(search.replace('?id=', ''))
+
+let movieId = search.replace('?id=', '')
+
+// Récupérer les détails d'un film
+function movieDetails(id) {
+    console.log('id');
+    axios .get('https://api.themoviedb.org/3/movie/' + id + '?api_key=e5be04ec7de9aff432b14905a60c0bb8')
+        .then((response) => {
+            let movie = response.data;
+            console.log(movie);
+            let parent = document.querySelector('main');
+            let film = document.createElement('div');
+            film.innerHTML = `
+                <div>
+                    <h1>${movie.title}</h1>
+                    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Poster">
+                    <p>${movie.overview}</p>
+                    <p>Release date: ${movie.release_date}</p>
+                    <p>Vote average: ${movie.vote_average}</p>
+                    <p>Vote count: ${movie.vote_count}</p>
+                    <p>Popularity: ${movie.popularity}</p>
+                    <p>Original language: ${movie.original_language}</p>
+                </div>`
+            parent.appendChild(film);
+        })
+}
+
+movieDetails(movieId);
