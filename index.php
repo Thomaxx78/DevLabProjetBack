48d0268a1baa455734cf7f73bf93012af31fd08f
Thomas Fi <tom.filhol@gmail.com>
2022-12-26 18:08:24 +0100

    :sparkles: nav and tri designs

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 require 'require/head.php'
 ?>
 <body>
-    <header class="w-full bg-darkgrey mt-0 mb-auto py-2">
+    <header class="z-40 sticky w-full bg-darkgrey mt-0 mb-auto py-2">
         <?php require 'require/nav.php' ?>
     </header>
     <div class="flex">
-        <div class="h-full bg-semidarkgrey lg:w-96 lg:ml-0 lg:mr-auto lg:pt-8  ">
-            <!--<div class="hidden lg:block">
-                <h2 class=" text-white text-3xl text-center font-semibold">Découvrir:</h2>
-                <p class="text-2xl text-white ml-8 lg:mt-4">Trier par:</p>
-                <p class="lg:ml-16 text-lightgrey text-xl mt-2">Noms</p>
-                <p class="lg:ml-16 text-lightgrey text-xl mt-2">Notes</p>
-                <h3 class="text-2xl text-white ml-8 mt-16 lg:mt-8">Catégorie:</h3>
-                <ul class="ml-16 grid grid-cols-3 text-lightgrey mt-4 text-xl">
+        <div class="hidden lg:grid h-full fixed bg-semidarkgrey lg:w-96 lg:ml-0 lg:mr-auto lg:pt-28 bottom-0 lg:grid-rows-[10%_22%_10%_40%_18%]">
+                <h2 class=" text-white text-3xl text-center font-semibold m-auto row-span-1">Découvrir:</h2>
+                <div>
+                    <p class="text-2xl text-white ml-8 mt-2">Trier par:</p>
+                    <p class="lg:ml-16 text-lightgrey text-xl mt-2">Noms</p>
+                    <p class="lg:ml-16 text-lightgrey text-xl mt-2">Notes</p>
+                </div>
+                <h3 class="text-2xl text-white ml-8 my-auto">Catégorie:</h3>
+                <ul class="ml-16 grid grid-cols-3 text-lightgrey text-xl">
                     <li>Horreur</li>
                     <li>Horreur</li>
                     <li>Horreur</li>
@@ -34,11 +35,9 @@ require 'require/head.php'
                     <li>Horreur</li>
                     <li>Horreur</li>
                 </ul>
-                <a href="#" class="py-1 px-2 rounded-lg bg-white  text-semidarkgrey font-semibold text-xl ml-auto">Rechercher</a>
+                <a href="#" class="h-10 py-1 px-2 rounded-lg bg-white text-semidarkgrey font-semibold text-xl mx-auto mt-4">Rechercher</a>
                 <div>
                 <script type="module" src="js/main.js"></script>
--->
-                </div>
             </div>
         <div class="flex-col ml-4 lg:hidden">
             <h2 class="text-white text-2xl m-auto">Films tendances</h2>
